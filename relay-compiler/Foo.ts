4dd98c5fc91bf009c63c0c7734d1466bccf30399
utgwkk <utagawakiki@gmail.com>
2022-06-06 22:34:27 +0900

    relay-compiler: test query

---
+++ relay-compiler/Foo.ts
@@ -0,0 +1,67 @@
+import { graphql } from "react-relay";
+
+const fooOkFragment = graphql`
+  fragment Foo_foo_ok on Foo {
+    id
+    bar {
+      ... on Bar1 {
+        __typename
+        barField
+        bar1Field
+      }
+      ... on Bar2 {
+        __typename
+        barField
+        bar2Field
+      }
+    }
+  }
+`;
+
+const barOkFragment = graphql`
+  fragment Foo_bar_ok on Bar {
+    ... on Bar1 {
+      __typename
+      barField
+      bar1Field
+    }
+    ... on Bar2 {
+      __typename
+      barField
+      bar2Field
+    }
+  }
+`;
+
+const fooNgFragment1 = graphql`
+  fragment Foo_foo_ng1 on Foo {
+    id
+    bar {
+      barField
+      __typename
+      ... on Bar1 {
+        bar1Field
+      }
+      ... on Bar2 {
+        bar2Field
+      }
+    }
+  }
+`;
+
+const fooNgFragment2 = graphql`
+  fragment Foo_foo_ng2 on Foo {
+    id
+    bar {
+      barField
+      ... on Bar1 {
+        __typename
+        bar1Field
+      }
+      ... on Bar2 {
+        __typename
+        bar2Field
+      }
+    }
+  }
+`;
